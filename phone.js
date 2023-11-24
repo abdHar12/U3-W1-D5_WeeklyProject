6fc8984c36e6c80a94c2b1807c88669d72c2f573
abdHar12 <145681190+abdHar12@users.noreply.github.com>
2023-11-24 19:21:16 +0100

    secon commit

--- phone.js
+++ phone.js
@@ -1,55 +1,137 @@
-var User = /** @class */ (function () {
-    function User(_carica, _numeroChiamate, _durataChiamate) {
+var PhoneUser = /** @class */ (function () {
+    function PhoneUser(_carica, _numeroChiamate, _durataChiamate) {
         this.carica = _carica;
         this.numeroChiamate = _numeroChiamate;
         this.durataChiamate = _durataChiamate;
     }
-    User.prototype.ricarica = function (unaRicarica) {
+    PhoneUser.prototype.ricarica = function (unaRicarica) {
         this.carica += unaRicarica;
     };
-    User.prototype.chiamata = function (minutiDurata) {
+    PhoneUser.prototype.chiamata = function (minutiDurata) {
         this.carica -= minutiDurata * 0.2;
         this.numeroChiamate++;
         this.durataChiamate.push(minutiDurata);
     };
-    User.prototype.numero404 = function () {
+    PhoneUser.prototype.numero404 = function () {
         return Number(this.carica.toFixed(3));
     };
-    User.prototype.getNumeroChiamate = function () {
+    PhoneUser.prototype.getNumeroChiamate = function () {
         return this.numeroChiamate;
     };
-    User.prototype.azzeraChiamate = function () {
+    PhoneUser.prototype.azzeraChiamate = function () {
         this.numeroChiamate = 0;
         this.durataChiamate = [];
     };
-    return User;
+    return PhoneUser;
 }());
-var FirstUser = new User(0, 0, []);
-var secondUser = new User(0, 0, []);
-var thirdUser = new User(0, 0, []);
+var FirstUser = new PhoneUser(0, 0, []);
 FirstUser.ricarica(20);
-console.log("Credito Residuo: " + FirstUser.numero404());
-FirstUser.chiamata(10);
-FirstUser.chiamata(3);
-console.log("Il numero di chiamate effettuate: " + FirstUser.getNumeroChiamate());
-console.log("Credito Residuo: " + FirstUser.numero404());
-console.log(FirstUser.durataChiamate);
-secondUser.ricarica(30);
-console.log("Credito Residuo 2nd User: " + secondUser.numero404());
-secondUser.chiamata(20);
-secondUser.chiamata(10);
-secondUser.chiamata(30);
-console.log("Il numero di chiamate effettuate dal 2nd User: " +
-    secondUser.getNumeroChiamate());
-console.log("Credito Residuo 2nd User: " + secondUser.numero404());
-console.log(secondUser.durataChiamate);
-thirdUser.ricarica(15);
-console.log("Credito Residuo 2nd User: " + thirdUser.numero404());
-thirdUser.chiamata(2);
-thirdUser.chiamata(5);
-thirdUser.chiamata(15);
-thirdUser.chiamata(11);
-console.log("Il numero di chiamate effettuate dal 3th User: " +
-    thirdUser.getNumeroChiamate());
-console.log("Credito Residuo 3th User: " + thirdUser.numero404());
-console.log(thirdUser.durataChiamate);
+var totalSeconds = 0;
+var refreshIntervalId;
+var divTime = document.getElementById("div-time");
+var interruptCall = document.getElementById("interrupt-call");
+var registerScreen = document.getElementById("calls-register");
+var button404 = document.getElementById("call-404");
+var spanRemainingTime = document.querySelector("#remaining-credit span");
+var divRemainingCredit = document.getElementById("remaining-credit");
+var a = document.getElementById("return-previews");
+var callScreen = document.getElementById("call-screen");
+var callButton = document.getElementById("call-button");
+var interruptCallButtonAction = function () {
+    var minutesLabel = document.getElementById("minutes");
+    var secondsLabel = document.getElementById("seconds");
+    var numOfCalls = document.getElementById("number-calls");
+    numOfCalls.getElementsByTagName("span")[0].innerHTML = " ".concat(FirstUser.getNumeroChiamate(), " ");
+    console.log("".concat(minutesLabel.innerHTML, ":").concat(secondsLabel.innerHTML));
+    FirstUser.chiamata(totalSeconds / 60);
+    console.log(typeof (totalSeconds / 60));
+    interruptCall.style.display = "none";
+    divTime.style.display = "none";
+    callButton.disabled = false;
+    totalSeconds = 0;
+    clearInterval(refreshIntervalId);
+    var callDiv = document.createElement("div");
+    callDiv.className = "d-flex justify-content-between px-2";
+    registerScreen.appendChild(callDiv);
+    var span1 = document.createElement("span");
+    span1.innerText = "Chiamata";
+    var span2 = document.createElement("span");
+    span2.innerText = "".concat(minutesLabel.innerHTML, ":").concat(secondsLabel.innerHTML);
+    callDiv.appendChild(span1);
+    callDiv.appendChild(span2);
+};
+var button404Action = function () {
+    divRemainingCredit.style.display = "block";
+    spanRemainingTime.innerHTML = "Il credito residuo \u00E8 di: ".concat(FirstUser.numero404().toFixed(2), "\u20AC");
+    console.log(FirstUser.numero404());
+};
+button404.onclick = button404Action;
+a.onclick = function () {
+    callScreen.style.display = "none";
+    registerScreen.style.display = "block";
+};
+var goCallButton = document.getElementById("go-call");
+goCallButton.onclick = function () {
+    callScreen.style.display = "block";
+    registerScreen.style.display = "none";
+};
+callButton.addEventListener("click", function () { return callButtonIstructions(callButton); });
+interruptCall.addEventListener("click", interruptCallButtonAction);
+var callButtonIstructions = function (callButton) {
+    divRemainingCredit.style.display = "none";
+    callButton.setAttribute("disabled", "true");
+    divTime.style.display = "block";
+    refreshIntervalId = setInterval(setTime, 10);
+    interruptCall.style.display = "block";
+    function setTime() {
+        var minutesLabel = document.getElementById("minutes");
+        var secondsLabel = document.getElementById("seconds");
+        ++totalSeconds;
+        secondsLabel.innerHTML = pad(totalSeconds % 60);
+        minutesLabel.innerHTML = pad(Number(Math.trunc(totalSeconds / 60)));
+    }
+    function pad(val) {
+        var valString = val + "";
+        if (valString.length < 2) {
+            return "0" + valString;
+        }
+        else {
+            return valString;
+        }
+    }
+};
+// const FirstUser = new PhoneUser(0, 0, []);
+// const secondUser = new PhoneUser(0, 0, []);
+// const thirdUser = new PhoneUser(0, 0, []);
+// FirstUser.ricarica(20);
+// console.log("Credito Residuo: " + FirstUser.numero404());
+// FirstUser.chiamata(10);
+// FirstUser.chiamata(3);
+// console.log(
+//   "Il numero di chiamate effettuate: " + FirstUser.getNumeroChiamate()
+// );
+// console.log("Credito Residuo: " + FirstUser.numero404());
+// console.log(FirstUser.durataChiamate);
+// secondUser.ricarica(30);
+// console.log("Credito Residuo 2nd User: " + secondUser.numero404());
+// secondUser.chiamata(20);
+// secondUser.chiamata(10);
+// secondUser.chiamata(30);
+// console.log(
+//   "Il numero di chiamate effettuate dal 2nd User: " +
+//     secondUser.getNumeroChiamate()
+// );
+// console.log("Credito Residuo 2nd User: " + secondUser.numero404());
+// console.log(secondUser.durataChiamate);
+// thirdUser.ricarica(15);
+// console.log("Credito Residuo 2nd User: " + thirdUser.numero404());
+// thirdUser.chiamata(2);
+// thirdUser.chiamata(5);
+// thirdUser.chiamata(15);
+// thirdUser.chiamata(11);
+// console.log(
+//   "Il numero di chiamate effettuate dal 3th User: " +
+//     thirdUser.getNumeroChiamate()
+// );
+// console.log("Credito Residuo 3th User: " + thirdUser.numero404());
+// console.log(thirdUser.durataChiamate);
--- phone.ts
+++ phone.ts
@@ -4,7 +4,7 @@ interface Smartphone {
   durataChiamate: number[];
 }
 
-class User implements Smartphone {
+class PhoneUser implements Smartphone {
   carica: number;
   numeroChiamate: number;
   durataChiamate: number[];
@@ -38,41 +38,135 @@ class User implements Smartphone {
   }
 }
 
-const FirstUser = new User(0, 0, []);
-const secondUser = new User(0, 0, []);
-const thirdUser = new User(0, 0, []);
-
+const FirstUser = new PhoneUser(0, 0, []);
 FirstUser.ricarica(20);
-console.log("Credito Residuo: " + FirstUser.numero404());
-FirstUser.chiamata(10);
-FirstUser.chiamata(3);
-console.log(
-  "Il numero di chiamate effettuate: " + FirstUser.getNumeroChiamate()
-);
-console.log("Credito Residuo: " + FirstUser.numero404());
-console.log(FirstUser.durataChiamate);
+let totalSeconds = 0;
+let refreshIntervalId;
+const divTime = document.getElementById("div-time") as HTMLLabelElement;
+const interruptCall = document.getElementById(
+  "interrupt-call"
+) as HTMLButtonElement;
+const registerScreen = document.getElementById(
+  "calls-register"
+) as HTMLDivElement;
+const button404 = document.getElementById("call-404") as HTMLButtonElement;
+const spanRemainingTime = document.querySelector(
+  "#remaining-credit span"
+) as HTMLSpanElement;
+const divRemainingCredit = document.getElementById(
+  "remaining-credit"
+) as HTMLSpanElement;
+const a = document.getElementById("return-previews") as HTMLAnchorElement;
+const callScreen = document.getElementById("call-screen") as HTMLDivElement;
+const callButton = document.getElementById("call-button") as HTMLButtonElement;
 
-secondUser.ricarica(30);
-console.log("Credito Residuo 2nd User: " + secondUser.numero404());
-secondUser.chiamata(20);
-secondUser.chiamata(10);
-secondUser.chiamata(30);
-console.log(
-  "Il numero di chiamate effettuate dal 2nd User: " +
-    secondUser.getNumeroChiamate()
-);
-console.log("Credito Residuo 2nd User: " + secondUser.numero404());
-console.log(secondUser.durataChiamate);
+const interruptCallButtonAction = (): void => {
+  const minutesLabel = document.getElementById("minutes") as HTMLLabelElement;
+  const secondsLabel = document.getElementById("seconds") as HTMLLabelElement;
 
-thirdUser.ricarica(15);
-console.log("Credito Residuo 2nd User: " + thirdUser.numero404());
-thirdUser.chiamata(2);
-thirdUser.chiamata(5);
-thirdUser.chiamata(15);
-thirdUser.chiamata(11);
-console.log(
-  "Il numero di chiamate effettuate dal 3th User: " +
-    thirdUser.getNumeroChiamate()
-);
-console.log("Credito Residuo 3th User: " + thirdUser.numero404());
-console.log(thirdUser.durataChiamate);
+  const numbCallsSpan = document.querySelector(
+    "#number-calls span"
+  ) as HTMLSpanElement;
+  numbCallsSpan.innerHTML = `${FirstUser.getNumeroChiamate()}`;
+  console.log(`${minutesLabel.innerHTML}:${secondsLabel.innerHTML}`);
+  FirstUser.chiamata(totalSeconds / 60);
+  console.log(typeof (totalSeconds / 60));
+  interruptCall.style.display = "none";
+  divTime.style.display = "none";
+  callButton.disabled = false;
+  totalSeconds = 0;
+  clearInterval(refreshIntervalId);
+  const callDiv = document.createElement("div") as HTMLDivElement;
+  callDiv.className = "d-flex justify-content-between px-2";
+  registerScreen.appendChild(callDiv);
+  const span1 = document.createElement("span") as HTMLDivElement;
+  span1.innerText = "Chiamata";
+  const span2 = document.createElement("span") as HTMLDivElement;
+  span2.innerText = `${minutesLabel.innerHTML}:${secondsLabel.innerHTML}`;
+  callDiv.appendChild(span1);
+  callDiv.appendChild(span2);
+  console.log(FirstUser.getNumeroChiamate());
+};
+const button404Action = () => {
+  divRemainingCredit.style.display = "block";
+
+  spanRemainingTime.innerHTML = `Il credito residuo è di: ${FirstUser.numero404().toFixed(
+    2
+  )}€`;
+  console.log(FirstUser.numero404());
+};
+
+a.onclick = () => {
+  callScreen.style.display = "none";
+  registerScreen.style.display = "block";
+};
+
+const goCallButton = document.getElementById("go-call") as HTMLButtonElement;
+goCallButton.onclick = () => {
+  callScreen.style.display = "block";
+  registerScreen.style.display = "none";
+};
+
+const callButtonIstructions = (callButton: HTMLButtonElement): void => {
+  divRemainingCredit.style.display = "none";
+  callButton.setAttribute("disabled", "true");
+  divTime.style.display = "block";
+  refreshIntervalId = setInterval(setTime, 10);
+  interruptCall.style.display = "block";
+  function setTime() {
+    const minutesLabel = document.getElementById("minutes") as HTMLLabelElement;
+    const secondsLabel = document.getElementById("seconds") as HTMLLabelElement;
+    ++totalSeconds;
+    secondsLabel.innerHTML = pad(totalSeconds % 60);
+    minutesLabel.innerHTML = pad(Number(Math.trunc(totalSeconds / 60)));
+  }
+};
+function pad(val: number) {
+  let valString = val + "";
+  if (valString.length < 2) {
+    return "0" + valString;
+  } else {
+    return valString;
+  }
+}
+callButton.addEventListener("click", () => callButtonIstructions(callButton));
+interruptCall.addEventListener("click", interruptCallButtonAction);
+button404.onclick = button404Action;
+// const FirstUser = new PhoneUser(0, 0, []);
+// const secondUser = new PhoneUser(0, 0, []);
+// const thirdUser = new PhoneUser(0, 0, []);
+
+// FirstUser.ricarica(20);
+// console.log("Credito Residuo: " + FirstUser.numero404());
+// FirstUser.chiamata(10);
+// FirstUser.chiamata(3);
+// console.log(
+//   "Il numero di chiamate effettuate: " + FirstUser.getNumeroChiamate()
+// );
+// console.log("Credito Residuo: " + FirstUser.numero404());
+// console.log(FirstUser.durataChiamate);
+
+// secondUser.ricarica(30);
+// console.log("Credito Residuo 2nd User: " + secondUser.numero404());
+// secondUser.chiamata(20);
+// secondUser.chiamata(10);
+// secondUser.chiamata(30);
+// console.log(
+//   "Il numero di chiamate effettuate dal 2nd User: " +
+//     secondUser.getNumeroChiamate()
+// );
+// console.log("Credito Residuo 2nd User: " + secondUser.numero404());
+// console.log(secondUser.durataChiamate);
+
+// thirdUser.ricarica(15);
+// console.log("Credito Residuo 2nd User: " + thirdUser.numero404());
+// thirdUser.chiamata(2);
+// thirdUser.chiamata(5);
+// thirdUser.chiamata(15);
+// thirdUser.chiamata(11);
+// console.log(
+//   "Il numero di chiamate effettuate dal 3th User: " +
+//     thirdUser.getNumeroChiamate()
+// );
+// console.log("Credito Residuo 3th User: " + thirdUser.numero404());
+// console.log(thirdUser.durataChiamate);
